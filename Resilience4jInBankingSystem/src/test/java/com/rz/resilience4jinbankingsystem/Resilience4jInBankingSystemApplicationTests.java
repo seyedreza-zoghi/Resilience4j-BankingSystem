e1fe50590e5f076fd30177dc103d0189a5790509
seyedreza zoghi <seyedreza.zoghi@gmailcom>
2024-12-29 08:33:50 +0330

    new project

---
+++ Resilience4jInBankingSystem/src/test/java/com/rz/resilience4jinbankingsystem/Resilience4jInBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rz.resilience4jinbankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jInBankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
