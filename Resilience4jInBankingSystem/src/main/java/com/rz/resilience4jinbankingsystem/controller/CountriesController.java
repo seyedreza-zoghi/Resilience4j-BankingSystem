8b82c2e805243c3befdb9f7489fddc3a9e92f5d3
seyedreza zoghi <seyedreza.zoghi@gmailcom>
2024-12-30 16:20:28 +0330

    feat: add call external service time limiter

--- Resilience4jInBankingSystem/src/main/java/com/rz/resilience4jinbankingsystem/controller/CountriesController.java
+++ Resilience4jInBankingSystem/src/main/java/com/rz/resilience4jinbankingsystem/controller/CountriesController.java
@@ -5,11 +5,13 @@ import com.rz.resilience4jinbankingsystem.service.CountriesService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class CountriesController {
@@ -40,12 +42,29 @@ public class CountriesController {
         throw new RuntimeException("Service failed");
     }
 
+    @GetMapping("/timeLimiter")
+    @TimeLimiter(name = "callExternalServiceTimeLimiter", fallbackMethod = "fallbackResponseTimeLimiter")
+    public CompletableFuture<String> callExternalService() {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Success!";
+        });
+    }
+
+
     public String fallbackResponse(Exception e) {
         return "Fallback response: " + e.getMessage();
     }
 
     public List<Object> restClientException(Throwable throwable) {
         throw new RestClientException("this not available . . .");
+    }
+    public CompletableFuture<String> fallbackResponseTimeLimiter(Throwable t) {
+        return CompletableFuture.completedFuture("Fallback response: " + t.getMessage());
     }
 
 }
